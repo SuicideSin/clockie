e2295e2d1b0fcbaa026d7b0ee45c13dcf0d6b061
Doug McInnes <doug@dougmcinnes.com>
2015-09-12 13:45:00 -0700

    blank until button

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 ARDMK_DIR = /usr/local/opt/arduino-mk
 
-BOARD = Microduino
-OBJDIR = build-microduino
+# BOARDS_TXT = /Applications/Arduino.app/Contents/Resources/Java/hardware/Microduino/boards.txt
+# BOARD_TAG = 328p8m
 
 include $(ARDMK_DIR)/Arduino.mk
--- clockie.ino
+++ clockie.ino
@@ -1,19 +1,61 @@
 #include <LiquidCrystal.h>
+#include <Time.h>
+
+unsigned int showCounter = 0;
+unsigned int lastSecond  = 60;
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void renderTime() {
+  unsigned int hr  = hour();
+  unsigned int min = minute();
+  unsigned int sec = second();
+  lcd.setCursor(0, 1);
+  if (hr < 10) {
+    lcd.print(0);
+  }
+  lcd.print(hr);
+  lcd.print(':');
+  if (min < 10) {
+    lcd.print(0);
+  }
+  lcd.print(min);
+  lcd.print(':');
+  if (sec < 10) {
+    lcd.print(0);
+  }
+  lcd.print(sec);
+}
+
+void showTime() {
+  lcd.display();
+  renderTime();
+  showCounter = 5;
+}
+
+void loop() {
+  if (showCounter > 0) {
+    unsigned int sec = second();
+    if (sec != lastSecond) {
+      lastSecond = sec;
+      showCounter--;
+      renderTime();
+      if (showCounter < 1) {
+        lcd.noDisplay();
+      }
+    }
+  }
+}
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("hello, world!");
-}
 
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+  lcd.noDisplay();
+
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(0, showTime, FALLING);
 }
