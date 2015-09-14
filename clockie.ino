658fa5ca260ef4d5b4f3cf9c5afdf6198d41bdd3
Doug McInnes <doug@dougmcinnes.com>
2015-09-13 22:17:46 -0700

    query router for date

--- clockie.ino
+++ clockie.ino
@@ -24,6 +24,8 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 unsigned int showCounter = 0;
 unsigned int lastSecond  = 60;
 
+uint32_t ip, gateway;
+
 // initialize the library with the numbers of the interface pins
 //                A0  A1  A2  A3  A4  A5
 LiquidCrystal lcd(14, 15, 16, 17, 18, 19);
@@ -104,7 +106,8 @@ void initCC3000() {
     delay(100); // ToDo: Insert a DHCP timeout!
   }
 
-  uint32_t ip, netmask, gateway, dhcpserv, dnsserv;
+  // don't care about these
+  uint32_t netmask, dhcpserv, dnsserv;
   cc3000.getIPAddress(&ip, &netmask, &gateway, &dhcpserv, &dnsserv);
 
   lcd.clear();
@@ -115,6 +118,47 @@ void initCC3000() {
   lcd.print((uint8_t)(ip >> 8));
   lcd.print('.');
   lcd.print((uint8_t)(ip));
+}
+
+void queryTime() {
+  lcd.clear();
+  lcd.print("Querying..");
+
+  Adafruit_CC3000_Client www = cc3000.connectTCP(gateway, 80);
+  if (www.connected()) {
+    www.fastrprint(F("GET / HTTP/1.1\r\n"));
+    www.fastrprint(F("HOST: ")); www.fastrprint("192.168.0.1"); www.fastrprint(F("\r\n"));
+    www.fastrprint(F("\r\n"));
+    www.println();
+  } else {
+    lcd.clear();
+    lcd.print("Connection failed");
+    return;
+  }
+
+  lcd.clear();
+  /* Read data until either the connection is closed, or the idle timeout is reached. */
+  String line = "";
+  unsigned long lastRead = millis();
+  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+    while (www.available()) {
+      char c = www.read();
+      if (c == '\n') {
+        if (line.startsWith("Date:")) {
+          lcd.print(line);
+        }
+        line = "";
+      } else {
+        line += c;
+      }
+      lastRead = millis();
+    }
+  }
+  www.close();
+
+  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
+  /* the next time your try to connect ... */
+  cc3000.disconnect();
 }
 
 void setup() {
@@ -135,99 +179,6 @@ void setup() {
   turnOnDisplay();
 
   initCC3000();
+
+  queryTime();
 }
-
-uint32_t ip;
-
-/* void xsetup(void) { */
-/*   Serial.begin(115200); */
-/*   Serial.println(F("Hello, CC3000!\n"));  */
-/*  */
-/*   Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC); */
-/*    */
-/*   #<{(| Initialise the module |)}># */
-/*   Serial.println(F("\nInitializing...")); */
-/*   if (!cc3000.begin()) */
-/*   { */
-/*     Serial.println(F("Couldn't begin()! Check your wiring?")); */
-/*     while(1); */
-/*   } */
-/*    */
-/*   // Optional SSID scan */
-/*   // listSSIDResults(); */
-/*    */
-/*   Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID); */
-/*   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { */
-/*     Serial.println(F("Failed!")); */
-/*     while(1); */
-/*   } */
-/*     */
-/*   Serial.println(F("Connected!")); */
-/*    */
-/*   #<{(| Wait for DHCP to complete |)}># */
-/*   Serial.println(F("Request DHCP")); */
-/*   while (!cc3000.checkDHCP()) */
-/*   { */
-/*     delay(100); // ToDo: Insert a DHCP timeout! */
-/*   }   */
-/*  */
-/*   #<{(| Display the IP address DNS, Gateway, etc. |)}>#   */
-/*   while (! displayConnectionDetails()) { */
-/*     delay(1000); */
-/*   } */
-/*  */
-/*   ip = 0; */
-/*   // Try looking up the website's IP address */
-/*   Serial.print(WEBSITE); Serial.print(F(" -> ")); */
-/*   while (ip == 0) { */
-/*     if (! cc3000.getHostByName(WEBSITE, &ip)) { */
-/*       Serial.println(F("Couldn't resolve!")); */
-/*     } */
-/*     delay(500); */
-/*   } */
-/*  */
-/*   cc3000.printIPdotsRev(ip); */
-/*    */
-/*   // Optional: Do a ping test on the website */
-/*   #<{(| */
-/*   Serial.print(F("\n\rPinging ")); cc3000.printIPdotsRev(ip); Serial.print("...");   */
-/*   replies = cc3000.ping(ip, 5); */
-/*   Serial.print(replies); Serial.println(F(" replies")); */
-/*   |)}>#   */
-/*  */
-/*   #<{(| Try connecting to the website. */
-/*      Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read. */
-/*   |)}># */
-/*   Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80); */
-/*   if (www.connected()) { */
-/*     www.fastrprint(F("GET ")); */
-/*     www.fastrprint(WEBPAGE); */
-/*     www.fastrprint(F(" HTTP/1.1\r\n")); */
-/*     www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n")); */
-/*     www.fastrprint(F("\r\n")); */
-/*     www.println(); */
-/*   } else { */
-/*     Serial.println(F("Connection failed"));     */
-/*     return; */
-/*   } */
-/*  */
-/*   Serial.println(F("-------------------------------------")); */
-/*    */
-/*   #<{(| Read data until either the connection is closed, or the idle timeout is reached. |)}>#  */
-/*   unsigned long lastRead = millis(); */
-/*   while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) { */
-/*     while (www.available()) { */
-/*       char c = www.read(); */
-/*       Serial.print(c); */
-/*       lastRead = millis(); */
-/*     } */
-/*   } */
-/*   www.close(); */
-/*   Serial.println(F("-------------------------------------")); */
-/*    */
-/*   #<{(| You need to make sure to clean up after yourself or the CC3000 can freak out |)}># */
-/*   #<{(| the next time your try to connect ... |)}># */
-/*   Serial.println(F("\n\nDisconnecting")); */
-/*   cc3000.disconnect(); */
-/*    */
-/* } */
